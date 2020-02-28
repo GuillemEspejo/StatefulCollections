aab6161267d3202a5e17158ab2a7ec6fd74a729e
Guillem Espejo <guillem.espejo@luciddreamer.io>
2020-02-28 14:32:45 +0100

    Added collectionviews, refactored state classes, modified readme.md

---
+++ StatefulCollections/Source/StatefulCollectionView.swift
@@ -0,0 +1,9 @@
+//
+//  StatefulCollectionView.swift
+//  StatefulCollections
+//
+//  Created by Guillem Espejo on 28/02/2020.
+//  Copyright © 2020 GEspejo. All rights reserved.
+//
+
+import Foundation
